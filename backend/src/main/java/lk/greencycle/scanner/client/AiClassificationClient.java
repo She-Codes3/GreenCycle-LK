8404f4f38a0019fb90df514cdd5f369f2b97e907
Sanduni69 <bandarasanduni69@gmail.com>
2026-09-07 10:50:40 +0530

    fix(backend): remove unused HTTP headers import

--- backend/src/main/java/lk/greencycle/scanner/client/AiClassificationClient.java
+++ backend/src/main/java/lk/greencycle/scanner/client/AiClassificationClient.java
@@ -2,7 +2,6 @@ package lk.greencycle.scanner.client;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
